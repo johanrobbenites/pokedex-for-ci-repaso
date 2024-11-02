ed52e36b4f4e34f0396c088d5674c8b775c5d674
unknown <johan.robles.b@uni.pe>
2024-11-02 03:31:13 -0500

    SetUp Node

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
-const root = createRoot(document.getElementById('root'))
+const rootElement = document.getElementById('app');
+const root = createRoot(rootElement);
 
-ReactDOM.root.render(<App />)
+root.render(<App />);
