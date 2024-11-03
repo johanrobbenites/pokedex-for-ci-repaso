fc4e47a49384b71653d02fd8154d5de562085fdd
unknown <johan.robles.b@uni.pe>
2024-11-03 04:42:52 -0500

    Pipline Testing

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-const rootElement = document.getElementById('app')
-const root = createRoot(rootElement)
-
-root.render(<App />)
+ReactDOM.render(<App />, document.getElementById('app'))
